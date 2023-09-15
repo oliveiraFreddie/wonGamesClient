031667aba3b93e1878523a0fc2d5a46b1d6fc999
oliveiraFreddie <freddie.oliveira95@gmail.com>
2023-09-15 16:03:17 +0100

    Button done and tested

---
+++ src/components/Button/stories.tsx
@@ -0,0 +1,30 @@
+import { StoryFn, Meta } from '@storybook/react'
+import { AddShoppingCart } from '@styled-icons/material-outlined/AddShoppingCart'
+import Button from '.'
+
+export default {
+  title: 'Button',
+  component: Button,
+  argTypes: {
+    children: {
+      type: 'string'
+    },
+    icon: {
+      table: { disable: true }
+    }
+  }
+} as Meta
+
+export const Default: StoryFn = (args) => <Button {...args} />
+
+Default.args = {
+  children: 'Buy now'
+}
+
+export const withIcon: StoryFn = (args) => <Button {...args} />
+
+withIcon.args = {
+  size: 'small',
+  children: 'Buy now',
+  icon: <AddShoppingCart />
+}
