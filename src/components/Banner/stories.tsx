99bc5c0d18e28129ba7d8d70d8deef86656ea239
oliveiraFreddie <freddie.oliveira95@gmail.com>
2023-09-19 11:34:32 +0100

    Banner and Ribbon done and tested

---
+++ src/components/Banner/stories.tsx
@@ -0,0 +1,40 @@
+import { Meta, StoryFn } from '@storybook/react'
+import Banner, { BannerProps } from '.'
+
+export default {
+  title: 'Banner',
+  component: Banner,
+  argTypes: {
+    ribbon: {
+      type: 'string'
+    }
+  },
+  args: {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+    title: 'Defy death',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/game/defy-death'
+  },
+  parameters: {
+    layout: 'fullscreen'
+  }
+} as Meta
+
+export const Default: StoryFn<BannerProps> = (args) => (
+  <div style={{ maxWidth: '104rem', margin: '0 auto' }}>
+    <Banner {...args} />
+  </div>
+)
+
+export const WithRibbon: StoryFn<BannerProps> = (args) => (
+  <div style={{ maxWidth: '104rem', margin: '0 auto' }}>
+    <Banner {...args} />
+  </div>
+)
+
+WithRibbon.args = {
+  ribbon: '20% OFF',
+  ribbonSize: 'normal',
+  ribbonColor: 'primary'
+}
