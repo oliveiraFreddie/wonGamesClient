e1a27a3f0b6a9018d0713512ccaa4c544be5c2a8
oliveiraFreddie <freddie.oliveira95@gmail.com>
2023-10-01 13:17:28 +0100

    Fix storybook

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -4,9 +4,9 @@ import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outline
 import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
 import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
 
-import Button from '@/components/Button'
-import Logo from '@/components/Logo'
-import MediaMatch from '@/components/MediaMatch'
+import Button from '../Button'
+import Logo from '../Logo'
+import MediaMatch from '../MediaMatch'
 import * as S from './styles'
 
 export type MenuProps = {
